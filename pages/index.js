aa8d7f6d98b5a46629c87510148461e7f44c08f6
Serkan Karaarslan <sekanet@gmail.com>
2019-08-09 11:13:59 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Layout from '../components/MyLayout.js';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+const Index = props => {
+  return (
+    <Layout>
+      <h1>Batman TV Shows</h1>
+      <ul>
+        {props.shows.map(show => {
+          return (
+            <li key={show.id}>
+              <Link href="/p/[id]" as={`/p/${show.id}`}>
+                <a>{show.name}</a>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </Layout>
+  )
+};
+
+Index.getInitialProps = async function(){
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count : ${data.length}`);
+
+  return {
+    shows : data.map(entry => entry.show)
+  };
+};
+
+export default Index;
---
+++ pages/p/[id].js
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router'
+import Layout from '../../components/MyLayout.js'
+import fetch from 'isomorphic-unfetch'
+
+const Post = props => {
+  return (
+    <Layout>
+      <h1>{props.show.name}</h1>
+      <p>{props.show.summary.replace(/<[/]?p>/g,'')}</p>
+      <img src={props.show.image.medium} />
+    </Layout>
+  );
+}
+
+
+Post.getInitialProps = async function(context) {
+  const {id} = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`fetched show : ${show.name}`);
+
+  return { show };
+
+};
+
+export default Post;
